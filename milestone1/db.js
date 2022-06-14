3dba3812375cc8194e6310d5a1c071aab973fe4d
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-14 15:53:58 +0200

    tentativi overflow-auto

--- milestone1/db.js
+++ milestone1/db.js
@@ -168,4 +168,26 @@ const contatti = [
         },
         ],
     },
+    {
+        name: "Davide",
+        avatar: "img/avatar_8.jpg",
+        visible: true,
+        messages: [
+        {
+            date: "10/01/2020 15:30:55",
+            message: "Ciao, andiamo a mangiare la pizza stasera?",
+            status: "received",
+        },
+        {
+            date: "10/01/2020 15:50:00",
+            message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+            status: "sent",
+        },
+        {
+            date: "10/01/2020 15:51:00",
+            message: "OK!!",
+            status: "received",
+        },
+        ],
+    },
 ];
