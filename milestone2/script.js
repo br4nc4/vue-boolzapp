7e405835c6dfa69fe3f3e552aef6df85d3cad09c
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-15 13:48:17 +0200

    tentativo milestone2 pt.2

--- milestone2/script.js
+++ milestone2/script.js
@@ -2,11 +2,32 @@ new Vue({
     el:"#app",
     data:{
         listaContatti: contatti,
-        currentContact: 0,
+        currentUser: {
+            name: "Michele",
+            avatar: "img/avatar_1.jpg",
+            visible: true,
+            messages: [
+            {
+                date: "10/01/2020 15:30:55",
+                message: "Hai portato a spasso il cane?",
+                status: "sent",
+            },
+            {
+                date: "10/01/2020 15:50:00",
+                message: "Ricordati di stendere i panni",
+                status: "sent",
+            },
+            {
+                date: "10/01/2020 16:15:22",
+                message: "Tutto fatto!",
+                status: "received",
+            },
+            ],
+        },
     },
     methods:{
         activeContact: function(currentContact){
-            return this.listaContatti[currentContact].messages;
+            return console.log(this.currentUser = currentContact); 
         }
     }
 });
