16bb283963187107d6b4206fd2d9941a78791e3c
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-16 13:34:48 +0200

    milestone 3 funciton not working

--- milestone3/script.js
+++ milestone3/script.js
@@ -3,11 +3,20 @@ new Vue({
     data:{
         listaContatti: contatti,
         currentUser: 0,
+        enterMessage:"",
     },
     methods:{
         activeContact: function(currentContact){
             this.currentUser = currentContact;
-    },
+        },
+        
+        inputMessage: function(){
+            this.listaContatti[this.currentUser].messages.push({
+                message: this.enterMessage,
+                status: "sent",
+            })
+            this.enterMessage ="";
+        },
     }
 });
 
